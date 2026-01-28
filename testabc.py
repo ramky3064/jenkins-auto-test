af7b30dca435114f3a5db91f172ed1ebe7c7fad2
Ramky <ramakrishna.peddhada@gmail.com>
2026-01-28 14:14:32 +0530

    new code

---
+++ testabc.py
@@ -0,0 +1,4 @@
+print("Hello, world!")  
+print("This is a test file named test0123.py")
+print("It contains simple print statements.")
+print("End of the test file.")
---
+++ testcba.py
@@ -0,0 +1,4 @@
+print("Goodbye, world!")
+print("This line is added to test the completion.")
+print("Final line of the test file.")
+print("Just another line for testing purposes.")
